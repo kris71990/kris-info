294803de163ed7c4c32d414489e7bb0a09556d48
Kris Sakarias <ksakarias@gmail.com>
2019-01-14 15:52:42 -0800

    skills page layout

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('click', function(e) {
   if (e.target.localName !== 'a') return;
 
-  var elementsLi = document.getElementsByTagName('a');
+  var elementsLi = document.querySelectorAll('nav a');
   var elementsMainSection = document.getElementsByTagName('section');
 
   for (var i = 0; i < elementsLi.length; i++) {
@@ -15,18 +15,4 @@ document.addEventListener('click', function(e) {
       elementsMainSection[i].className = 'hidden';
     }
   }
-  
-  // var route = e.target.textContent.toLowerCase();
-  // if (route.includes(' ')) {
-  //   route = route.split(' ')[0]
-  // }
-
-  // var contentToggled = document.getElementById('body').innerHTML;
-  // window.history.pushState({ 'html' : contentToggled }, e.target.textContent, '/' + route);
-  // window.onpopstate = function(e) {
-  //   if (e.state) {
-  //     document.getElementById('body').innerHTML = e.state.html;
-  //   }
-  // }
-  // console.log(history);
 }, false);
