ad08349696874f654bba2fc6a681c6619a33e581
Kris Sakarias <ksakarias@gmail.com>
2019-01-24 12:32:56 -0800

    project page construction, toggling projects by language

--- js/index.js
+++ js/index.js
@@ -1,18 +1,41 @@
-document.addEventListener('click', function(e) {
+var navMenu = document.getElementById('nav');
+var langMenu = document.getElementById('language-selection');
+
+navMenu.addEventListener('click', function(e) {
   if (e.target.localName !== 'a') return;
 
-  var elementsLi = document.querySelectorAll('nav a');
+  var elementsHeaderLi = document.querySelectorAll('nav a');
   var elementsMainSection = document.getElementsByTagName('section');
 
-  for (var i = 0; i < elementsLi.length; i++) {
-    if (elementsLi[i] === e.target) {
+  for (var i = 0; i < elementsHeaderLi.length; i++) {
+    if (elementsHeaderLi[i] === e.target) {
       e.target.parentNode.className = 'selected';
       elementsMainSection[i].className = 'visible';
 
       
     } else {
-      elementsLi[i].parentNode.className = '';
+      elementsHeaderLi[i].parentNode.className = '';
       elementsMainSection[i].className = 'hidden';
+    }
+  }
+}, false);
+
+langMenu.addEventListener('click', function(e) {
+  if (e.target.localName !== 'a') return;
+
+  var targetLang = e.target.textContent.toLowerCase();
+  var langChoicesA = document.querySelectorAll('#language-selection a');
+  var langChoicesLi = document.querySelectorAll('#language-selection li');
+  var langDivs = document.querySelectorAll('section.visible > div');
+
+  for (var j = 0; j < langDivs.length; j++) {
+    langDivs[j].className = langChoicesA[j].textContent.toLowerCase();
+    if (langDivs[j].className !== targetLang) {
+      langChoicesLi[j].className = '';
+      langDivs[j].className = 'hidden';
+    } else {
+      langChoicesLi[j].className = 'selected';
+      langDivs[j].className = targetLang;
     }
   }
 }, false);
